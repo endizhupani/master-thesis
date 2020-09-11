f88f08b388ba3679bea83e83bb0278961f419985
Endi Zhupani <endizhupani@gmail.com>
2020-09-11 17:17:19 +0200

    modified gigignore

---
+++ omp_parallelized_solver/build/CMakeFiles/omp_parallelized_solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/omp_parallelized_solver.dir/main.cpp.o"
+  "omp_parallelized_solver"
+  "omp_parallelized_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/omp_parallelized_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- omp_parallelized_solver/main.cpp
+++ omp_parallelized_solver/main.cpp
@@ -2,16 +2,15 @@
 #include <math.h>
 #include <omp.h>
 
-#define N 100
+#define N 1000
 #define EPSILON 0.01
 
 int getChunkSize(int arraySize);
 
 int main() {
-    int n_threads = 4;
+    int n_threads = 4, i, j;
     printf("Running on max %d threads.\n", n_threads);
     omp_set_num_threads(n_threads);
-    double *diff;
     double mean;
     double *u = new double[N*N];
     double *w = new double[N*N];
@@ -19,20 +18,20 @@ int main() {
 
     mean = 0,0;
 
-#pragma omp parallel default(none) shared(u,w) reduction(+:mean)
+#pragma omp parallel default(none) private(i) shared(u,w) reduction(+:mean)
     {
 #pragma omp for schedule(static, getChunkSize(N*N))
-    for (int i = 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         u[i * N] = u[i * N + (N - 1)] = u[i] = w[i * N] = w[i * N + (N - 1)] = w[i] = 100;
         u[(N - 1) * N + i] = w[(N - 1) * N + i] = 0;
         mean += u[i * N] + u[i * N + (N - 1)] + u[i] + u[(N - 1) * N + i];
     }
 }
     mean /= (4 * N);
-#pragma omp parallel default(none) shared(u) firstprivate(mean)
+#pragma omp parallel default(none) shared(u) private(i) firstprivate(mean)
     {
 #pragma omp for schedule(static, getChunkSize(N*N))
-    for(int i = N; i< (N*N) - N; i++){
+    for(i = N; i< (N*N) - N; i++){
         if (i%N == 0 || (i-(N-1))%N == 0){
             continue;
         }
@@ -44,67 +43,40 @@ int main() {
     double calc_loop_min = 1000000;
     double calc_loop_max = 0;
     double calc_loop_avg = 0;
-    double val_assign_loop_min = 1000000;
-    double val_assign_loop_max = 0;
-    double val_assign_loop_avg = 0;
 
     double start = omp_get_wtime();
     int num_iter = 0;
     for (;;) {
         num_iter++;
-        diff = new double[omp_get_max_threads()];
         double globalDiff = 0.0;
         double calc_loop_start = omp_get_wtime();
-#pragma omp parallel default(none) firstprivate(u,w) reduction(max:globalDiff)
+#pragma omp parallel default(none) private(i) firstprivate(u,w, num_iter) //reduction(max:globalDiff)
         {
             #pragma omp for schedule(static, getChunkSize(N*N))
-            for (int i = N + 1; i < (N * N) - N - 1; i++) {
+            for (i = N + 1; i < (N * N) - N - 1; i++) {
                 if (i % N == 0 || (i - (N - 1)) % N == 0) {
                     continue;
                 }
-                w[i] = (u[i - 1] + u[i + 1] + u[i - N] + u[i + N]) / 4;
-                if (fabs(w[i] - u[i]) > globalDiff) {
-                    globalDiff = fabs(w[i] - u[i]);
-                }
+                w[i] = num_iter + u[i];
+                //w[i] = (u[i - 1] + u[i + 1] + u[i - N] + u[i + N]) / 4;
+//                if (fabs(w[i] - u[i]) > globalDiff) {
+//                    globalDiff = fabs(w[i] - u[i]);
+//                }
+//globalDiff = 4;
             }
         }
-        double calc_loop_dur = omp_get_wtime() - calc_loop_start;
-        calc_loop_avg += calc_loop_dur;
-        if (calc_loop_dur < calc_loop_min){
-            calc_loop_min = calc_loop_dur;
-        }
-
-        if (calc_loop_dur > calc_loop_max){
-            calc_loop_max = calc_loop_dur;
-        }
-//        for (int i = 0; i < omp_get_max_threads(); i++){
-//            if (diff[i] > globalDiff){
-//                globalDiff = diff[i];
-//            }
-//        }
-
-        if (globalDiff <= EPSILON) break;
-//        double val_assign_start = omp_get_wtime();
-////#pragma omp parallel default(none) firstprivate(u, w)
-////        {
-////#pragma omp for //schedule(static, getChunkSize(N*N))
-////            for(int i = N; i< (N*N) - N; i++){
-////                if (i%N == 0 || (i-(N-1))%N == 0){
-////                    continue;
-////                }
-////                u[i] = w[i];
-////            }
-////        }
-//
-//        double val_assign_dur = omp_get_wtime() - val_assign_start;
-//        val_assign_loop_avg += val_assign_dur;
-//        if (val_assign_dur < val_assign_loop_min){
-//            val_assign_loop_min = val_assign_dur;
+//        double calc_loop_dur = omp_get_wtime() - calc_loop_start;
+//        calc_loop_avg += calc_loop_dur;
+//        if (calc_loop_dur < calc_loop_min){
+//            calc_loop_min = calc_loop_dur;
 //        }
 //
-//        if (val_assign_dur > val_assign_loop_max){
-//            val_assign_loop_max = val_assign_dur;
+//        if (calc_loop_dur > calc_loop_max){
+//            calc_loop_max = calc_loop_dur;
 //        }
+
+        if (num_iter >= 1815 ) break;
+
         tmp = u;
         u = w;
         w = tmp;
@@ -117,10 +89,6 @@ int main() {
     printf("calc_loop_min: %f\n", calc_loop_min);
     printf("calc_loop_max: %f\n", calc_loop_max);
     printf("calc_loop_avg: %f\n", calc_loop_avg / num_iter);
-
-    printf("val_assign_loop_min: %f\n", val_assign_loop_min);
-    printf("val_assign_loop_max: %f\n", val_assign_loop_max);
-    printf("val_assign_loop_avg: %f\n", val_assign_loop_avg / (num_iter-1));
 //    for(int i = 0; i < N; i++){
 //        for (int j = 0; j < N; j++) {
 //            printf("%6.2f ", u[i*N + j]);
