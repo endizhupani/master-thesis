6ec4ec1655f8d90ef0fe0a09c2e0af9069b3aa24
Endi Zhupani <endizhupani@gmail.com>
2020-09-11 19:04:43 +0200

    parallel solver with matrix as array

--- omp_parallelized_solver/main.cpp
+++ omp_parallelized_solver/main.cpp
@@ -7,38 +7,42 @@
 
 int getChunkSize(int arraySize);
 
-int main() {
-    int n_threads = 4, i, j;
+int main()
+{
+    int n_threads = 1, i, j;
     printf("Running on max %d threads.\n", n_threads);
     omp_set_num_threads(n_threads);
     double mean;
-    double *u = new double[N*N];
-    double *w = new double[N*N];
+    double *u = new double[N * N];
+    double *w = new double[N * N];
     double *tmp;
 
-    mean = 0,0;
+    mean = 0, 0;
 
-#pragma omp parallel default(none) private(i) shared(u,w) reduction(+:mean)
+#pragma omp parallel default(none) private(i) shared(u, w) reduction(+ \
+                                                                     : mean)
     {
-#pragma omp for schedule(static, getChunkSize(N*N))
-    for (i = 0; i < N; i++) {
-        u[i * N] = u[i * N + (N - 1)] = u[i] = w[i * N] = w[i * N + (N - 1)] = w[i] = 100;
-        u[(N - 1) * N + i] = w[(N - 1) * N + i] = 0;
-        mean += u[i * N] + u[i * N + (N - 1)] + u[i] + u[(N - 1) * N + i];
+#pragma omp for schedule(static, getChunkSize(N *N))
+        for (i = 0; i < N; i++)
+        {
+            u[i * N] = u[i * N + (N - 1)] = u[i] = w[i * N] = w[i * N + (N - 1)] = w[i] = 100;
+            u[(N - 1) * N + i] = w[(N - 1) * N + i] = 0;
+            mean += u[i * N] + u[i * N + (N - 1)] + u[i] + u[(N - 1) * N + i];
+        }
     }
-}
     mean /= (4 * N);
 #pragma omp parallel default(none) shared(u) private(i) firstprivate(mean)
     {
-#pragma omp for schedule(static, getChunkSize(N*N))
-    for(i = N; i< (N*N) - N; i++){
-        if (i%N == 0 || (i-(N-1))%N == 0){
-            continue;
+#pragma omp for schedule(static, getChunkSize(N *N))
+        for (i = N; i < (N * N) - N; i++)
+        {
+            if (i % N == 0 || (i - (N - 1)) % N == 0)
+            {
+                continue;
+            }
+            u[i] = mean;
         }
-        u[i] = mean;
     }
-
-}
 
     double calc_loop_min = 1000000;
     double calc_loop_max = 0;
@@ -46,41 +50,47 @@ int main() {
 
     double start = omp_get_wtime();
     int num_iter = 0;
-    for (;;) {
+    for (;;)
+    {
         num_iter++;
         double globalDiff = 0.0;
         double calc_loop_start = omp_get_wtime();
-#pragma omp parallel default(none) private(i) firstprivate(u,w, num_iter) //reduction(max:globalDiff)
+#pragma omp parallel default(none) private(i) firstprivate(u, w, num_iter) reduction(max \
+                                                                                     : globalDiff)
         {
-            #pragma omp for schedule(static, getChunkSize(N*N))
-            for (i = N + 1; i < (N * N) - N - 1; i++) {
-                if (i % N == 0 || (i - (N - 1)) % N == 0) {
+#pragma omp for schedule(static, getChunkSize(N *N))
+            for (i = N + 1; i < (N * N) - N - 1; i++)
+            {
+                if (i % N == 0 || (i - (N - 1)) % N == 0)
+                {
                     continue;
                 }
-                w[i] = num_iter + u[i];
-                //w[i] = (u[i - 1] + u[i + 1] + u[i - N] + u[i + N]) / 4;
-//                if (fabs(w[i] - u[i]) > globalDiff) {
-//                    globalDiff = fabs(w[i] - u[i]);
-//                }
-//globalDiff = 4;
+
+                w[i] = (u[i - 1] + u[i + 1] + u[i - N] + u[i + N]) / 4;
+                if (fabs(w[i] - u[i]) > globalDiff)
+                {
+                    globalDiff = fabs(w[i] - u[i]);
+                }
             }
         }
-//        double calc_loop_dur = omp_get_wtime() - calc_loop_start;
-//        calc_loop_avg += calc_loop_dur;
-//        if (calc_loop_dur < calc_loop_min){
-//            calc_loop_min = calc_loop_dur;
-//        }
-//
-//        if (calc_loop_dur > calc_loop_max){
-//            calc_loop_max = calc_loop_dur;
-//        }
+        double calc_loop_dur = omp_get_wtime() - calc_loop_start;
+        calc_loop_avg += calc_loop_dur;
+        if (calc_loop_dur < calc_loop_min)
+        {
+            calc_loop_min = calc_loop_dur;
+        }
 
-        if (num_iter >= 1815 ) break;
+        if (calc_loop_dur > calc_loop_max)
+        {
+            calc_loop_max = calc_loop_dur;
+        }
+
+        if (globalDiff <= EPSILON)
+            break;
 
         tmp = u;
         u = w;
         w = tmp;
-
     }
 
     double end = omp_get_wtime();
@@ -89,19 +99,20 @@ int main() {
     printf("calc_loop_min: %f\n", calc_loop_min);
     printf("calc_loop_max: %f\n", calc_loop_max);
     printf("calc_loop_avg: %f\n", calc_loop_avg / num_iter);
-//    for(int i = 0; i < N; i++){
-//        for (int j = 0; j < N; j++) {
-//            printf("%6.2f ", u[i*N + j]);
-//        }
-//        putchar('\n');
-//    }
-    delete [] u;
-    delete [] w;
+    //    for(int i = 0; i < N; i++){
+    //        for (int j = 0; j < N; j++) {
+    //            printf("%6.2f ", u[i*N + j]);
+    //        }
+    //        putchar('\n');
+    //    }
+    delete[] u;
+    delete[] w;
 
     printf("Solved\n");
 }
 
-int getChunkSize(int arraySize){
+int getChunkSize(int arraySize)
+{
     int num_threads = omp_get_num_threads();
-    return ceil((double)arraySize/num_threads);
+    return ceil((double)arraySize / num_threads);
 }
--- sequential_pde_solver/main.cpp
+++ sequential_pde_solver/main.cpp
@@ -2,33 +2,39 @@
 #include <math.h>
 #include <chrono>
 
-#define N 10000
+#define N 1000
 #define EPSILON 0.01
 
-int main() {
+int main()
+{
     double diff, mean;
     int i, j;
-    double **u = new double*[N];
-    for (i = 0; i < N; i++) {
+    double **u = new double *[N];
+    for (i = 0; i < N; i++)
+    {
         u[i] = new double[N];
     }
 
-    double **w = new double*[N];
-    for (i = 0; i < N; i++) {
+    double **w = new double *[N];
+    for (i = 0; i < N; i++)
+    {
         w[i] = new double[N];
     }
 
-    mean = 0,0;
-    for (i = 0; i < N; i++) {
-        u[i][0] = u[i][N-1] = u[0][i] = 100;
-        u[N-1][i] = 0;
-        mean += u[i][0] + u[i][N-1] + u[0][i] + u[N-1][i];
+    mean = 0, 0;
+    for (i = 0; i < N; i++)
+    {
+        u[i][0] = u[i][N - 1] = u[0][i] = 100;
+        u[N - 1][i] = 0;
+        mean += u[i][0] + u[i][N - 1] + u[0][i] + u[N - 1][i];
     }
 
     mean /= (4 * N);
 
-    for(i = 1; i < N-1; i++){
-        for (j = 1; j < N-1; j++) {
+    for (i = 1; i < N - 1; i++)
+    {
+        for (j = 1; j < N - 1; j++)
+        {
             u[i][j] = mean;
         }
     }
@@ -41,49 +47,59 @@ int main() {
     double val_assign_loop_min = 1000000;
     double val_assign_loop_max = 0;
     double val_assign_loop_avg = 0;
-    for (;;) {
+    for (;;)
+    {
         num_iter++;
-        diff = 0,0;
+        diff = 0, 0;
 
         auto calc_loop_start = std::chrono::high_resolution_clock::now();
-        for (i = 1; i < N-1; i++) {
-            for (j = 1; j < N-1; j++) {
-                w[i][j] = (u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1])/4;
+        for (i = 1; i < N - 1; i++)
+        {
+            for (j = 1; j < N - 1; j++)
+            {
+                w[i][j] = (u[i - 1][j] + u[i + 1][j] + u[i][j - 1] + u[i][j + 1]) / 4;
 
-                if (fabs(w[i][j] - u[i][j]) > diff){
+                if (fabs(w[i][j] - u[i][j]) > diff)
+                {
                     diff = fabs(w[i][j] - u[i][j]);
                 }
             }
         }
         auto calc_loop_end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> calc_loop_elapsed = calc_loop_end - calc_loop_start;
-        if (calc_loop_elapsed.count() > calc_loop_max){
+        if (calc_loop_elapsed.count() > calc_loop_max)
+        {
             calc_loop_max = calc_loop_elapsed.count();
         }
 
-        if (calc_loop_elapsed.count() < calc_loop_min){
+        if (calc_loop_elapsed.count() < calc_loop_min)
+        {
             calc_loop_min = calc_loop_elapsed.count();
         }
 
         calc_loop_avg += calc_loop_elapsed.count();
 
-
-        if (diff <= EPSILON) break;
+        if (diff <= EPSILON)
+            break;
 
         auto val_assign_loop_start = std::chrono::high_resolution_clock::now();
-        for(i = 1; i < N-1; i++){
-            for (j = 1; j < N-1; j++) {
+        for (i = 1; i < N - 1; i++)
+        {
+            for (j = 1; j < N - 1; j++)
+            {
                 u[i][j] = w[i][j];
             }
         }
         auto val_assign_loop_finish = std::chrono::high_resolution_clock::now();
 
         std::chrono::duration<double> val_assign_loop_elapsed = val_assign_loop_finish - val_assign_loop_start;
-        if (val_assign_loop_elapsed.count() > val_assign_loop_max){
+        if (val_assign_loop_elapsed.count() > val_assign_loop_max)
+        {
             val_assign_loop_max = val_assign_loop_elapsed.count();
         }
 
-        if (val_assign_loop_elapsed.count() < val_assign_loop_min){
+        if (val_assign_loop_elapsed.count() < val_assign_loop_min)
+        {
             val_assign_loop_min = val_assign_loop_elapsed.count();
         }
 
@@ -92,7 +108,7 @@ int main() {
 
     auto full_calc_finish = std::chrono::high_resolution_clock::now();
     printf("num_iter: %d\n", num_iter);
-    std::chrono::duration<double> elapsed =full_calc_finish - full_calc_start;
+    std::chrono::duration<double> elapsed = full_calc_finish - full_calc_start;
     printf("full_calc_time: %f\n", elapsed.count());
 
     printf("calc_loop_min: %f\n", calc_loop_min);
@@ -101,26 +117,28 @@ int main() {
 
     printf("val_assign_loop_min: %f\n", val_assign_loop_min);
     printf("val_assign_loop_max: %f\n", val_assign_loop_max);
-    printf("val_assign_loop_avg: %f\n", val_assign_loop_avg / (num_iter-1));
+    printf("val_assign_loop_avg: %f\n", val_assign_loop_avg / (num_iter - 1));
 
-//        for(i = 0; i < N; i++){
-//        for (j = 0; j < N; j++) {
-//            printf("%6.2f ", u[i][j]);
-//        }
-//        putchar('\n');
-//    }
+    //        for(i = 0; i < N; i++){
+    //        for (j = 0; j < N; j++) {
+    //            printf("%6.2f ", u[i][j]);
+    //        }
+    //        putchar('\n');
+    //    }
 
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < N; i++)
+    {
         delete[] u[i];
     }
 
-    delete [] u;
+    delete[] u;
 
-    for (i = 0; i < N; i++) {
-        delete [] w[i];
+    for (i = 0; i < N; i++)
+    {
+        delete[] w[i];
     }
 
-    delete [] w;
+    delete[] w;
 
-printf("Solved\n");
+    printf("Solved\n");
 }
